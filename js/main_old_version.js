5653789479b75611468251a7529409c89b971194
= <geison.sn@gmail.com>
2016-04-29 16:31:59 -0400

    Using classes with ES6

--- js/main_old_version.js
+++ js/main_old_version.js
@@ -1,14 +1,13 @@
-import Mortgage from './mortgage';
-
-document.getElementById('calcBtn').addEventListener('click', () => {
+import * as mortgage from './mortgage';
+document.getElementById('calcBtn').addEventListener('click', () => { 
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
-    let mortgage = new Mortgage(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = mortgage.monthlyPayment.toFixed(2);
-    document.getElementById("monthlyRate").innerHTML = (rate / 12).toFixed(2);
+    let {monthlyPayment, monthlyRate, amortization} = mortgage.calculateAmortization(principal, years, rate);
+    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
     let html = "";
-    mortgage.amortization.forEach((year, index) => html +=`
+    amortization.forEach((year, index) => html +=`
         <tr>
             <td>${index + 1}</td>
             <td class="currency">${Math.round(year.principalY)}</td>
@@ -17,7 +16,7 @@ document.getElementById('calcBtn').addEventListener('click', () => {
                     <div class="bar principal"
                         style="flex:${year.principalY};-webkit-flex:${year.principalY}">
                     </div>
-                    <div class="bar interest"
+                    <div class="bar interest" 
                         style="flex:${year.interestY};-webkit-flex:${year.interestY}">
                     </div>
                 </div>
@@ -26,4 +25,4 @@ document.getElementById('calcBtn').addEventListener('click', () => {
             <td class="currenty">${Math.round(year.balance)}</td>
     `);
     document.getElementById("amortization").innerHTML = html;
-});
+});        
